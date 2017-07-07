2317fea21a47f815236786b6cb81c8d40687feb0
赵 振 <zhaozhenyingyu@163.com>
2017-07-07 23:14:21 +0800

    2017/7/7日，初始工程第二次提交！添加初始化和输出函数！

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+#define MaxSize 50
+typedef struct List {
+    int data[MaxSize];
+    int length;
+}seqList;
 using namespace std;
+//方法定义
+void initList(seqList& L);
+void printList(seqList& L);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    cout<<"haha"<<endl;
+    seqList L;
+    initList(L);
+    printList(L);
+
     return 0;
+}
+
+//初始化线性表
+void initList(seqList& L){
+    srand((unsigned)time(NULL));//为rand()函数生成不同的随机种子
+    L.length=10;
+    for (int i = 0; i < L.length; ++i) {
+        L.data[i]=rand()%100;
+    }
+}
+
+void printList(seqList& L){
+    for (int i = 0; i < L.length; ++i) {
+        cout<<L.data[i]<<",";
+    }
 }
